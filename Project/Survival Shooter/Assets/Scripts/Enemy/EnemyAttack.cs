c039d8072443be0f2094d2068a7a9b273abbc2e9
Unknown <dydgus5120@gmail.com>
2018-04-05 15:32:39 +0900

    z

---
+++ Project/Survival Shooter/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+    Animator anim;
+    GameObject player;
+    PlayerHeath playerHeath;
+    bool playerInRange;
+    float timer;
+
+	// Use this for initialization
+	void Awake () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHeath = player.GetComponent<PlayerHeath>();
+        anim = GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange)
+        {
+            Attack();
+        }
+
+        if(playerHeath.currentHealth <= 0)
+        {
+            
+            anim.SetTrigger("PlayerDead");
+        }
+	}
+
+    void Attack()
+    {
+        timer = 0;
+
+        if(playerHeath.currentHealth > 0)
+        {
+            playerHeath.TakeDamage(attackDamage);
+        }
+    }
+}
