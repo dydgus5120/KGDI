19c9316958fa460798a0b43c459581821aab22be
Unknown <dydgus5120@gmail.com>
2018-04-27 16:52:31 +0900

    4-27

--- Project/Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Project/Survival Shooter/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -9,15 +9,23 @@ public class EnemyMovement : MonoBehaviour {
 
     NavMeshAgent nav;
 
+    PlayerHeath playerHeath;
+    EnemyHealth enemyHealth;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        playerHeath = player.GetComponent<PlayerHeath>();
+        enemyHealth = GetComponent<EnemyHealth>();
         nav = GetComponent<NavMeshAgent>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        nav.SetDestination(player.position);	
+        if (enemyHealth.currentHealth > 0 && playerHeath.currentHealth > 0)
+            nav.SetDestination(player.position);
+
+        else
+            nav.enabled = false;
 	}
 }
---
+++ Project/Survival Shooter/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public PlayerHeath playerHeath;
+    public float restartDelay = 5;
+
+    Animator anim;
+    float restartTime;
+
+	// Use this for initialization
+	void Awake () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(playerHeath.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTime += Time.deltaTime;
+
+            if(restartTime >= restartDelay)
+            {
+                Application.LoadLevel(0);
+            }
+        }
+
+	}
+}
